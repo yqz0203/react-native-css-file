4a114eea22df538347f985e7fa493a85574fe3bd
qizhang.yang <yangqizhang@medlinker.com>
2018-09-16 10:02:43 +0800

    chore: change package name

--- src/watch.js
+++ src/watch.js
@@ -4,15 +4,18 @@ const path = require('path');
 
 
 module.exports = (dir) => {
+  // tips: 这里必须使用绝对路径
   const watcher = chokidar.watch(`${path.resolve(dir)}/**/*.css`, {
     ignored: /node_modules/,
   });
 
   watcher
-    .on('add', path => {
-      buildCss(path);
+    .on('add', filePath => {
+      const relative = path.relative(path.resolve('.'), filePath);
+      buildCss(relative);
     })
-    .on('change', path => {
-      buildCss(path);
+    .on('change', filePath => {
+      const relative = path.relative(path.resolve('.'), filePath);
+      buildCss(relative);
     });
 };
